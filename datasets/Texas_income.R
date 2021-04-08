63f7464fbcce913ad089cec1cdb967d12ec74570
Claus Wilke <wilke@austin.utexas.edu>
2021-04-07 22:55:10 -0500

    new texas income dataset

--- datasets/Texas_income.R
+++ datasets/Texas_income.R
@@ -3,8 +3,11 @@ library(sf)
 library(here)
 
 texas_income <- practicalgg::texas_income %>%
+  st_drop_geometry() %>% 
+  extract(NAME, into = "county", regex = "(.+) County, Texas") %>%
   select(-variable) %>%
-  rename(median_income = estimate)
+  rename(median_income = estimate, FIPS = GEOID) %>%
+  st_set_geometry(st_geometry(practicalgg::texas_income))
 
 saveRDS(texas_income, here("datasets", "Texas_income.rds"))
 
