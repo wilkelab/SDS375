69c9bae22e8a48b19c09da548fd937ba41b4499d
Claus Wilke <wilke@austin.utexas.edu>
2022-01-13 20:16:08 -0600

    reorg

--- _deploy.sh
+++ _deploy.sh
@@ -4,5 +4,6 @@ Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
 rm -rf ./docs/slides/*.Rmd
 rm -rf ./docs/worksheets/*.html
 rm -rf ./docs/datasets/*.R
+rm -rf ./docs/archive
 git add ./docs/
 
