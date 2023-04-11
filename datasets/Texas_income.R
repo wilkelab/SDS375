2d502a77fae4af4162111a92f21ce9920d66b082
Claus Wilke <wilke@austin.utexas.edu>
2023-04-10 23:04:01 -0500

    update various lectures

--- datasets/Texas_income.R
+++ datasets/Texas_income.R
@@ -2,12 +2,17 @@ library(tidyverse)
 library(sf)
 library(here)
 
-texas_income <- practicalgg::texas_income %>%
-  st_drop_geometry() %>% 
-  extract(NAME, into = "county", regex = "(.+) County, Texas") %>%
-  select(-variable) %>%
-  rename(median_income = estimate, FIPS = GEOID) %>%
-  st_set_geometry(st_geometry(practicalgg::texas_income))
+## Old code that generated the dataset, required practicalgg
+#texas_income <- practicalgg::texas_income %>%
+#  st_drop_geometry() %>% 
+#  extract(NAME, into = "county", regex = "(.+) County, Texas") %>%
+#  select(-variable) %>%
+#  rename(median_income = estimate, FIPS = GEOID) %>%
+#  st_set_geometry(st_geometry(practicalgg::texas_income))
+
+## update to new-style crs object
+texas_income <- readRDS(here("datasets", "Texas_income.rds"))
+st_crs(texas_income) <- st_crs(4269)
 
 saveRDS(texas_income, here("datasets", "Texas_income.rds"))
 
